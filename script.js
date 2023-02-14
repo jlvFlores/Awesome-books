cd76244ebdf5dd9f49052dfdbd1066ca9547aba7
Jose Luis Vazquez Flores <jluisvazquez.jv@gmail.com>
2023-02-14 00:34:23 -0600

    Update JS functionality

--- script.js
+++ script.js
@@ -1,49 +1,53 @@
-/* eslint-disable no-unused-vars */
-const addButton = document.getElementById('add-btn');
-const library = document.getElementById('library');
+let books = [];
 
-let books = JSON.parse(localStorage.getItem('books'));
+if (localStorage.getItem('books')) {
+  books = JSON.parse(localStorage.getItem('books'));
+}
 
-function displayBooks() {
+const updateLocalStorage = () => localStorage.setItem('books', JSON.stringify(books));
+
+const reloadPage = () => document.location.reload(true);
+
+const displayBooks= () => {
+  const library = document.getElementById('library');
+  let count = 0;
   books.forEach((book) => {
+    book.Number = JSON.stringify(count);
     library.insertAdjacentHTML('beforeend', `
       <div>
         <p>${book.Title}</p>
         <p>${book.Author}</p>
-        <button onclick="removeBook('${book.Title}')">Remove</button>
+        <button class="remove-btn" data-id="${book.Number}">Remove</button>
       </div>
     `);
+    count += count;
   });
+
+  const removeBook = (id) => {
+    books = books.filter((book) => book.Number !== id);
+    updateLocalStorage();
+    reloadPage();
+  }
+
+  const removeButtons = document.querySelectorAll('.remove-btn');
+  removeButtons.forEach((button) => button.addEventListener('click', (e) => {
+    removeBook(e.target.dataset.id);
+  }));
 }
 
-function updateLocalStorage() {
-  localStorage.setItem('books', JSON.stringify(books));
-}
-
-function reloadPage() {
-  document.location.reload(true);
-}
-
-function addBook(title, author) {
+const addBook = (title, author) => {
   books.push({ Title: title, Author: author });
   updateLocalStorage();
   reloadPage();
 }
 
-function removeBook(title) {
-  books = books.filter((book) => book.Title !== title);
-  updateLocalStorage();
-  reloadPage();
-}
-
-function validateForm() {
+const addButton = document.getElementById('add-btn');
+addButton.addEventListener('click', () => {
   const inputTitle = document.getElementById('input-title').value;
   const inputAuthor = document.getElementById('input-author').value;
   if (inputTitle && inputAuthor) {
     addBook(inputTitle, inputAuthor);
   }
-}
+});
 
 displayBooks();
-
-addButton.addEventListener('click', validateForm);
